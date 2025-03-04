762f3a2247a0338a8b8af1fe5c97139b7a3d6731
imnxtamess <davide.morisco.work@gmail.com>
2025-03-04 15:10:26 +0100

    add readme.md file and prepared dom elements selection on js

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,13 @@
+// Preparation
 
-/*
-Coding Challenge Brief:
+// select the dom elements
 
-Titolo: Creazione di una To-Do List Interattiva
+const formEl = document.querySelector("form")
+const submitEl = document.querySelector(".addTaskBtn")
+const listEl = document.getElementById("list")
+const removeBtnEl = document.querySelector(".removeTaskBtn")
+const checkboxEl = document.querySelector(".checkbox")
 
-Descrizione:
-In questo coding challenge, creerai una semplice applicazione web di To-Do List utilizzando HTML, CSS e JavaScript. L'applicazione permetterà agli utenti di aggiungere, visualizzare e rimuovere elementi dalla lista delle cose da fare.
+// append a list item once the submitEl gets clicked
 
-Requisiti:
-1. HTML:
-   - Crea una struttura di base della pagina con un titolo e un contenitore per la lista delle cose da fare.
-   - Aggiungi un campo di input e un pulsante per permettere agli utenti di aggiungere nuovi elementi alla lista.
 
-2. CSS:
-   - Stila la pagina per renderla visivamente gradevole.
-   - Utilizza il CSS per evidenziare gli elementi completati della lista.
-
-3. JavaScript:
-   - Implementa la logica per aggiungere nuovi elementi alla lista quando l'utente clicca sul pulsante.
-   - Implementa la logica per rimuovere gli elementi dalla lista quando l'utente clicca su un pulsante di rimozione accanto a ciascun elemento.
-   - Implementa la logica per segnare gli elementi come completati quando l'utente clicca su di essi.
-
-Obiettivi di apprendimento:
-- Comprendere come manipolare il DOM utilizzando JavaScript.
-- Applicare stili CSS per migliorare l'aspetto visivo di una pagina web.
-- Utilizzare eventi JavaScript per interagire con gli elementi della pagina.
-
-Buon divertimento e buona programmazione!
-*/
