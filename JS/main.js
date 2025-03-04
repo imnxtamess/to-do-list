a730d5d3d86d0e2b501a4a51e754bc4776a9c4bb
imnxtamess <davide.morisco.work@gmail.com>
2025-03-04 15:18:43 +0100

    add user input click event to render markup on page

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,29 @@ const listEl = document.getElementById("list")
 const removeBtnEl = document.querySelector(".removeTaskBtn")
 const checkboxEl = document.querySelector(".checkbox")
 
-// append a list item once the submitEl gets clicked
+// append a list item to the listEl once the submitEl gets clicked
 
+submitEl.addEventListener("click", (e) => {
+   e.preventDefault() // prevents the form default
 
+   // save the user input into a variable
+   const toDoThing = document.querySelector("input").value
+
+   // 👇🏻 add the toDoThing inside the listEl
+
+   //👉🏻 initialize a variable that defines the needed markup
+
+   const markup = `<li class="d-flex gap-2 mb-3">
+              <div class="checkbox">
+                <span class="checkmark"><i class="fa-solid fa-check"></i></span>
+              </div>
+              <span class="task">${toDoThing}</span>
+              <span class="removeTaskBtn">
+                <i class="fa-solid fa-xmark"></i
+              ></span>
+            </li>`
+   //👉🏻 insert the markup on the dom inside the listEl
+
+   listEl.innerHTML += markup
+
+})
