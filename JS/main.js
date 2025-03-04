b3e85551bb18f60db6478c3d74d0b94510f07fd4
imnxtamess <davide.morisco.work@gmail.com>
2025-03-04 15:04:24 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+
+/*
+Coding Challenge Brief:
+
+Titolo: Creazione di una To-Do List Interattiva
+
+Descrizione:
+In questo coding challenge, creerai una semplice applicazione web di To-Do List utilizzando HTML, CSS e JavaScript. L'applicazione permetterà agli utenti di aggiungere, visualizzare e rimuovere elementi dalla lista delle cose da fare.
+
+Requisiti:
+1. HTML:
+   - Crea una struttura di base della pagina con un titolo e un contenitore per la lista delle cose da fare.
+   - Aggiungi un campo di input e un pulsante per permettere agli utenti di aggiungere nuovi elementi alla lista.
+
+2. CSS:
+   - Stila la pagina per renderla visivamente gradevole.
+   - Utilizza il CSS per evidenziare gli elementi completati della lista.
+
+3. JavaScript:
+   - Implementa la logica per aggiungere nuovi elementi alla lista quando l'utente clicca sul pulsante.
+   - Implementa la logica per rimuovere gli elementi dalla lista quando l'utente clicca su un pulsante di rimozione accanto a ciascun elemento.
+   - Implementa la logica per segnare gli elementi come completati quando l'utente clicca su di essi.
+
+Obiettivi di apprendimento:
+- Comprendere come manipolare il DOM utilizzando JavaScript.
+- Applicare stili CSS per migliorare l'aspetto visivo di una pagina web.
+- Utilizzare eventi JavaScript per interagire con gli elementi della pagina.
+
+Buon divertimento e buona programmazione!
+*/
